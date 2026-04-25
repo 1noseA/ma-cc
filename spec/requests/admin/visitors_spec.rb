656f4acf68f17e01b85ce518d5782d531040d7d5
1noseA <1noseA2L@gmail.com>
2026-04-26 00:18:25 +0900

    T06：管理画面の訪問者詳細に行動履歴の時系列表示を実装

--- spec/requests/admin/visitors_spec.rb
+++ spec/requests/admin/visitors_spec.rb
@@ -21,4 +21,20 @@ RSpec.describe "Admin::Visitors", type: :request do
       expect(response.body).to include(visitor.visitor_token)
     end
   end
+
+  describe "GET /admin/visitors/:id" do
+    it "訪問者詳細画面で行動履歴が時系列で表示される" do
+      visitor = create(:visitor)
+      older_event = create(:event, visitor: visitor, path: "/articles/1", occurred_at: 2.hours.ago)
+      newer_event = create(:event, visitor: visitor, path: "/articles/2", occurred_at: 1.hour.ago)
+
+      get admin_visitor_path(visitor)
+
+      expect(response).to have_http_status(:ok)
+      expect(response.body).to include("/articles/1")
+      expect(response.body).to include("/articles/2")
+      # 新しいイベントが先に表示される（時系列降順）
+      expect(response.body.index("/articles/2")).to be < response.body.index("/articles/1")
+    end
+  end
 end
