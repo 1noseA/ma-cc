64d868b9be90f3efbc7a14b706ad161b27591e76
1noseA <1noseA2L@gmail.com>
2026-04-28 19:37:50 +0900

    T07：トップページにお問い合わせフォームを表示

---
+++ spec/system/home_spec.rb
@@ -0,0 +1,13 @@
+require "rails_helper"
+
+RSpec.describe "Home", type: :system do
+  before { driven_by(:rack_test) }
+
+  it "トップページ(LP)に訪問するとフォームが表示される" do
+    create(:form)
+
+    visit root_path
+
+    expect(page).to have_css("form")
+  end
+end
