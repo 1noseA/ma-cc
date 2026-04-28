acf914f0244ca2dbc4d2f462046fa2fae266eabf
1noseA <1noseA2L@gmail.com>
2026-04-28 19:35:38 +0900

    T07：記事詳細ページにお問い合わせフォームを表示

---
+++ spec/system/articles_spec.rb
@@ -0,0 +1,14 @@
+require "rails_helper"
+
+RSpec.describe "Articles", type: :system do
+  before { driven_by(:rack_test) }
+
+  it "記事詳細ページに訪問するとフォームが表示される" do
+    form = create(:form)
+    article = create(:article)
+
+    visit article_path(article)
+
+    expect(page).to have_css("form")
+  end
+end
