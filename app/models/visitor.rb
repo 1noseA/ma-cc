e013980c73b017aec12b18e640009fd286765450
1noseA <1noseA2L@gmail.com>
2026-04-29 10:43:30 +0900

    T08：FormSubmission・Leadモデルとリード化フロー・管理画面を追加

--- app/models/visitor.rb
+++ app/models/visitor.rb
@@ -1,6 +1,7 @@
 class Visitor < ApplicationRecord
   has_many :events, dependent: :destroy
   has_many :form_submissions, dependent: :destroy
+  has_one :lead, dependent: :nullify
 
   def page_view_count
     events.page_views.count
