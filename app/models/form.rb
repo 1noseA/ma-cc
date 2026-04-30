acd3f3f14093cf174be4fd6bec43cf4bd0a9c9ba
1noseA <1noseA2L@gmail.com>
2026-04-30 09:29:34 +0900

    T09：Form モデルに表示条件チェックを追加

--- app/models/form.rb
+++ app/models/form.rb
@@ -1,5 +1,10 @@
 class Form < ApplicationRecord
   has_many :form_submissions, dependent: :destroy
+  has_many :display_rules, dependent: :destroy
 
   validates :name, presence: true
+
+  def should_display_for?(visitor)
+    display_rules.any? { |rule| rule.matches?(visitor) }
+  end
 end
